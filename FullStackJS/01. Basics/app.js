9552eee2f41fab8d5435971941d6864e81742a9d
danielgr765 <danielg.rivera00@gmail.com>
2024-05-13 11:48:34 -0600

    Primera Practica basica xd

--- FullStackJS/01. Basics/app.js
+++ FullStackJS/01. Basics/app.js
@@ -1,1 +1,19 @@
-alert("something is wrong");
+let name = "Daniel";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Male";
+let twitterHandle = "http://twitter.com";
+
+console.log(name);
+
+console.log(whatDoYouWannaBecomeInYourLife);
+
+console.log(gender);
+
+console.log(twitterHandle);
+
+console.table({
+  name: "Daniel",
+  whatDoYouWannaBecomeInYourLife: "programmer",
+  gender: "Male",
+  twitterHandle: "http://twitter.com",
+});
