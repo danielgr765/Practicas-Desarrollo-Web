8e00729eaced38deba372972bb48f9c207cf3a5e
danielgr765 <danielg.rivera00@gmail.com>
2024-05-14 11:43:06 -0600

    Callback Function

--- FullStackJS/01. Basics/app.js
+++ FullStackJS/01. Basics/app.js
@@ -1,22 +1,21 @@
+let num1 = 10;
+let num2 = 11;
 
-let favBands = ["Slipknot", "Avenged Sevenfold", "Disturbed"];
+function showCallFunc (x, y, fn){
+    
+    let pom = x * (fn(x, y));
 
-console.log(favBands[0]);
+    return pom;
+}
 
-let favNumbers = [1, 2, 3, 4];
-let mixexdArr = ["string", ["otherway1", "otherway2"], 123, true];
+function prueba1 (x, y, fn){
+    return x / (fn(x, y));
+}
 
-console.log(favNumbers[3]);
-console.log(mixexdArr[1][0]);
+function fn (x,y) {
+    let sum = x + y;
+    return sum;
+}
 
-// | Method     |                                      Description                                       |
-// | ---------- | :------------------------------------------------------------------------------------: |
-// | concat()   |                     joins two or more arrays and returns a result                      |
-// | includes() |                    checks if an array contains a specified element                     |
-// | push()     |    aads a new element to the end of an array and returns the new length of an array    |
-// | unshift()  | adds a new element to the beginning of an array and returns the new length of an array |
-// | pop()      |          removes the last element of an array and returns the removed element          |
-// | shift()    |         removes the first element of an array and returns the removed element          |
-// | sort()     |          sorts the elements alphabetically in strings and in ascending order           |
-// | slice()    |                 selects the part of an array and returns the new array                 |
-// | splice()   |             removes or replaces existing elements and/or adds new elements             |
+console.log(showCallFunc(num1, num2, fn));
+console.log(prueba1(num1, num2, fn));
